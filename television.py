1a5aebd75a67c5bf90c04a88fd8a51866c5606d7
alexorellana034 <142549570+alexorellana034@users.noreply.github.com>
2023-11-17 21:33:13 -0600

    Final

--- television.py
+++ television.py
@@ -1,20 +1,33 @@
 class Television:
     MIN_VOLUME = 0
+    MAX_VOLUME = 2
+    MIN_CHANNEL = 0
+    MAX_CHANNEL = 3
 
-    def __init__(self):
+    def __init__(self) -> None:
         self.__muted = False
         self.__volume = Television.MIN_VOLUME
+        self.__status = False
+        self.__channel = Television.MIN_CHANNEL
 
     def power(self):
-        pass
+        self.__status = not self.__status
+        
 
-    def mute(self):
-        pass
-
+    def mute(self) -> None:
+        self.__muted = not self.__muted
+    
     def channel_up(self):
-        pass
+         if self.__status:
+            if self.__channel < Television.MAX_CHANNEL:
+                self.__channel += 1
+            else:
+                self.__channel = Television.MIN_CHANNEL
 
     def channel_down(self):
+        """
+        Method to decrese the tv channel.
+        """
         if self.__status:
             if self.__channel > Television.MIN_CHANNEL:
                 self.__channel -= 1
@@ -24,16 +37,23 @@ class Television:
     def volume_up(self):
         if self.__status:
             self.__muted = False
-            if self.__valume < Television.MAX_VOLUME:
+            if self.__volume < Television.MAX_VOLUME:
                 self.__volume += 1
 
-    def valume_down(self):
-        pass
+    def volume_down(self):
+         if self.__status:
+            self.__muted = False
+            if self.__volume > Television.MIN_VOLUME:
+                self.__volume -= 1
 
-    def __str__(self):
+    def __str__(self) -> str:
+        """
+        Method to show the tv status
+        :return: tv status.
+        """
         if self.__muted:
-            return f'Volume = {Television.MIN_VOLUME}'
+            return f'Power= {self.__status}, Channel = {self.__channel}, Volume = {Television.MIN_VOLUME}'
         else:
-            return f'xxx'
+            return f'Power = {self.__status}, Channel = {self.__channel}, Volume = {self.__volume}'
 
 
